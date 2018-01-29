56eb6d0c21ba24b7de7e9747c9f61705dc79c50e
Nicholas Volkert <nvolkert@NVs-iMac.attlocal.net>
2018-01-28 23:59:38 -0600

    built out basic functionality

--- js/rps.js
+++ js/rps.js
@@ -12,8 +12,8 @@ firebase.initializeApp(config);
   var database = firebase.database();
 
   // Initial Values
-  var name = name;
-  var userGuess = $("#player1Choice button").attr("value");
+  var name = '';
+  var userGuess = $("#player1Choice button").val();
   var oponentGuess = $("#player2Choice button").val();
   // var guess = guess;
 
@@ -22,15 +22,16 @@ firebase.initializeApp(config);
     event.preventDefault();
     name = $("#userForm").val().trim();
     $("#startScreen1").hide();
+    $("#player1Turn .userName").text(name);
     database.ref().set({
       name: name
     });
-
+    $("#userForm").val('')
   });
 
   // Firebase watcher + initial loader HINT: .on("value")
   database.ref().on("value", function(snapshot) {
-    $("#player1Turn .userName").text(name);
+
 
     // Handle the errors
   }, function(errorObject) {
@@ -40,11 +41,39 @@ firebase.initializeApp(config);
   var losses = 0;
   var ties = 0;
   // var userGuess = $("#gameContainer button").text();
-
+  var buttonvalue = $(this).attr("value");
+  var buttonparent = $(this).parent();
   $("#player1Choice button").on("click", function() {
+    var buttonvalue = $(this).attr("value");
+    $("#player1Choice").hide();
+    $(".yourTurn").hide();
+    $("#player1Guess h3").text(buttonvalue);
 
-    $("#player1Guess").text(userGuess);
-    console.log("game container button click is working");
+    });
+    $("#player2Choice button").on("click", function() {
+      var buttonvalue = $(this).attr("value");
+      $("#player2Choice").hide();
+      $(".yourTurn").hide();
+      $("#player2Guess h3").text(buttonvalue);
+
+    });
+
+  // $("#gameContainer button").on("click", function() {
+  //   var buttonvalue = $(this).attr("value");
+  //   var buttonparent = $(this).parent();
+  //   console.log($(this).parent());
+  //   if ($(this).parent() === $("#player1Choice")){
+  //     $("#player1Choice").hide();
+  //     $(".yourTurn").hide();
+  //     $("#player1Guess h3").text(buttonvalue);
+  //     console.log("player 1 clicked")
+  //   }else{
+  //     $("#player2Choice").hide();
+  //     $(".yourTurn").hide();
+  //     $("#player2Guess h3").text(buttonvalue);
+  //     console.log("player 2 clicked")
+  //   }
+
     // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
     // if ((userGuess === "rock") || (userGuess === "paper") || (userGuess === "scissors")) {
     //
@@ -64,7 +93,7 @@ firebase.initializeApp(config);
     //     ties++;
     //   }
     // }
-  });
+
 
 //still working on:
 
@@ -73,5 +102,12 @@ firebase.initializeApp(config);
   //need to set show and hides of player cards based on the stage of the game
 
   //need to set trash talking so it's being sent and set in the database
+  // Capture Button Click
+  $("#trashButton").on("click", function(event) {
+    event.preventDefault();
+    trashTalk = $("#trashInput").val().trim();
+    $("#trashWindow").append('<p class="trash1">' + trashTalk + '</p>');
+    $("#trashInput").val('')
+  });
 
   //need to figure out how to set player two user name entry and player two choices
