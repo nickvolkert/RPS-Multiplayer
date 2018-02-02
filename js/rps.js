5031a088463a1f64dd9b382ade0c5863d94f52ce
Nicholas Volkert <nvolkert@NVs-iMac.attlocal.net>
2018-02-01 21:31:43 -0600

    progress so far

--- js/rps.js
+++ js/rps.js
@@ -15,18 +15,37 @@ firebase.initializeApp(config);
   var name = '';
   var userGuess = $("#player1Choice button").val();
   var oponentGuess = $("#player2Choice button").val();
+  var chatLog = '';
+  var wins = 0;
+  var losses = 0;
+  var plays = 0;
+  var turn = true;
   // var guess = guess;
-
+  var userLog = 0;
   // Capture Button Click
   $("#submitUserName").on("click", function(event) {
     event.preventDefault();
     name = $("#userForm").val().trim();
     $("#startScreen1").hide();
-    $("#player1Turn .userName").text(name);
-    database.ref().set({
-      name: name
-    });
-    $("#userForm").val('')
+      firebase.database().ref().push({
+        name: name,
+        userGuess: userGuess,
+        chatLog: chatLog,
+        wins: wins,
+        losses: losses,
+        plays: plays,
+        turn: turn,
+        userLog: userLog
+      });
+      userLog++;
+      $("#userEntry").hide();
+      if (userLog == 0){
+        $("#startScreen2").hide();
+        console.log("running user ONE entry");
+      } else if (userLog == 1){
+        $("#startScreen1").hide();
+        console.log("running user TWO entry");
+      }
   });
 
   // Firebase watcher + initial loader HINT: .on("value")
@@ -37,12 +56,14 @@ firebase.initializeApp(config);
   }, function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
   });
+
   var wins = 0;
   var losses = 0;
   var ties = 0;
   // var userGuess = $("#gameContainer button").text();
   var buttonvalue = $(this).attr("value");
   var buttonparent = $(this).parent();
+
   $("#player1Choice button").on("click", function() {
     var buttonvalue = $(this).attr("value");
     $("#player1Choice").hide();
@@ -58,21 +79,22 @@ firebase.initializeApp(config);
 
     });
 
-  // $("#gameContainer button").on("click", function() {
-  //   var buttonvalue = $(this).attr("value");
-  //   var buttonparent = $(this).parent();
-  //   console.log($(this).parent());
-  //   if ($(this).parent() === $("#player1Choice")){
-  //     $("#player1Choice").hide();
-  //     $(".yourTurn").hide();
-  //     $("#player1Guess h3").text(buttonvalue);
-  //     console.log("player 1 clicked")
-  //   }else{
-  //     $("#player2Choice").hide();
-  //     $(".yourTurn").hide();
-  //     $("#player2Guess h3").text(buttonvalue);
-  //     console.log("player 2 clicked")
-  //   }
+  $("#gameContainer button").on("click", function() {
+    var buttonvalue = $(this).attr("value");
+    var buttonparent = $(this).parent();
+    console.log($(this).parent());
+    if ($(this).parent() === $("#player1Choice")){
+      $("#player1Choice").hide();
+      $(".yourTurn").hide();
+      $("#player1Guess h3").text(buttonvalue);
+      console.log("player 1 clicked")
+    }else{
+      $("#player2Choice").hide();
+      $(".yourTurn").hide();
+      $("#player2Guess h3").text(buttonvalue);
+      console.log("player 2 clicked")
+    }
+  });
 
     // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
     // if ((userGuess === "rock") || (userGuess === "paper") || (userGuess === "scissors")) {
@@ -106,8 +128,18 @@ firebase.initializeApp(config);
   $("#trashButton").on("click", function(event) {
     event.preventDefault();
     trashTalk = $("#trashInput").val().trim();
-    $("#trashWindow").append('<p class="trash1">' + trashTalk + '</p>');
-    $("#trashInput").val('')
+    userOneTrash = $("#trashWindow").append('<p class="trash1">' + trashTalk + '</p>');
+    userTwoTrash = $("#trashWindow").append('<p class="trash2">' + trashTalk + '</p>');
+    if (userLog == 0){
+      userOneTrash;
+      chatLog.userOneTrash;
+      console.log("user one trash working");
+    } else {
+      userTwoTrash;
+      console.log("user two trash working");
+    }
+    $("#trashInput").val('');
+
   });
 
   //need to figure out how to set player two user name entry and player two choices
